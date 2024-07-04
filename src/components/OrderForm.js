215ba6ff24b2c845bbba5170b0ed1aa605ca9a70
John Doe <k3655692@gmail.com>
2024-07-04 20:45:44 +0300

    sdfsdf

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -25,7 +25,7 @@ const OrderForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/apply', {
+      const response = await fetch('https://veng-vnz.com/api/apply', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
